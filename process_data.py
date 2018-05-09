1acb4e992b0687992c11d147959b3860c0cdcbd6
AlexHu123 <446789948@qq.com>
2018-05-09 15:32:50 +0800

    delete unused code

--- process_data.py
+++
@@ -1,7 +0,0 @@
-f = open('data/ucf101/flow.txt', 'r')
-f1 = open('flow.txt', 'w')
-for line in f.readlines():
-    
-    f1.write(line.replace('/data4/zhouhao/dataset/ucf101/tvl1_flow/{:s}/', ''))
-f.close()
-f1.close()
